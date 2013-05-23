dc2568cffdcc767dd5a7a34f627bc3a0d6f319c4
vpsrini <VSrinivasa@Walmatrtlabs.com>
2013-05-23 14:44:28 -0700

    Added myJQuery Demo.

---
+++ MyJQuery/src/main/webapp/js/MyJQueryDemo.js
@@ -0,0 +1,39 @@
+/**
+ * @author :: 'Vara'
+ * This is the script file for JQuery.
+ * 
+ */
+//Power of closures in JavaScript.
+/*var x = 42; 
+console.log( x ); 
+var message = (function( x ){
+  return function() {
+    console.log( "x is " + x );
+  };
+})( x ); 
+message();
+x = 12; 
+console.log( x ); 
+message();*/
+
+//Hello world.
+/*jQuery*/$( document ).ready( function() {
+	
+	//$.extend demo -> combines/merge one or more objects and ties it with target object.
+	var targetObj = {'name' : 'VaraPrasad Nagalla'};
+	var locationObj = {
+		'street' : '2000 CRYSTAL SPRINGS RD',
+		'city'   : 'SAN BRUNO',
+		'state'  : 'CA',
+		'zip'    : '94066'
+	};
+	var updatedNameObj = {
+			'name' : 'VaraPrasad Srinivasa V V Nagalla',
+			'nickName' : 'Vara'
+	};
+	
+	//merges all three objects into target object.
+	$.extend(targetObj,locationObj,updatedNameObj);
+	console.log("Name* :: " + targetObj['name'] + " **Nick name : " + targetObj['nickName']);
+	
+});
