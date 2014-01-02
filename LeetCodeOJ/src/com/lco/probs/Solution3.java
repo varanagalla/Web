0ad948108c070fdea59fbfa2228403742eec9f52
Vara Srinivasa <VSrinivasa@walmartlabs.com>
2014-01-02 14:02:13 -0800

    Added solution to SortList with Merge Sort.

---
+++ LeetCodeOJ/src/com/lco/probs/Solution3.java
@@ -0,0 +1,85 @@
+/**
+ * 
+ */
+package com.lco.probs;
+
+/**
+ * @author vpsrini
+ *
+ */
+
+class ListNode {
+	 int val;
+	 ListNode next;
+	 ListNode(int x) {
+		 val = x;
+		 next = null;
+	 }
+	 
+	 
+}
+
+public class Solution3 {
+
+	
+	
+	public ListNode sortList(ListNode head) {
+		
+		if(head == null || head.next == null){
+			return head;
+		}
+		
+		ListNode ps = head;
+		ListNode pf = head.next;
+		
+		while(pf != null && pf.next != null){
+			ps = ps.next;
+			pf = pf.next.next;
+		}
+		ListNode second = ps.next;
+		ps.next = null;
+		
+		ListNode leftPart = this.sortList(head);
+		
+		ListNode rightPart = this.sortList(second);
+		
+		return merge(leftPart, rightPart);
+		
+    }
+	
+	
+	private ListNode merge(ListNode p, ListNode k){
+		ListNode head = new ListNode(-1);
+		ListNode l = head;
+		
+		while(p != null && k != null){
+			if(p.val < k.val){
+				l.next = p;
+				p = p.next;
+			}else{
+				l.next = k;
+				k = k.next;
+			}
+			l = l.next;
+		}
+		
+		if(p != null){
+			l.next = p;
+		}
+		
+		if(k != null){
+			l.next = k;
+		}
+		
+		return head.next;
+	}
+	
+	public void display(ListNode head){
+		while(head != null){
+			System.out.print(head.val);
+			System.out.print("\t");
+			head = head.next;
+		}
+	}
+
+}
---
+++ LeetCodeOJ/src/com/lco/probs/Solution3Test.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.lco.probs;
+
+
+/**
+ * @author vpsrini
+ *
+ */
+public class Solution3Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		ListNode head = new ListNode(-1);
+		addLinkNodes(head, new int[]{2,7,8,1,0,-1,-6});
+		
+		Solution3 sln3 = new Solution3();
+		System.out.println("Before Sort :: (See Below)");
+		sln3.display(head.next);
+		System.out.println();
+		
+		head = sln3.sortList(head.next);
+		System.out.println("After sort :: (See Below)");
+		sln3.display(head);
+		
+	}
+	
+	static void addLinkNodes(ListNode head, int[] elements){
+		ListNode c = head;
+		for(int element : elements){
+			c.next = new ListNode(element);
+			c = c.next;
+		}
+	}
+
+}
